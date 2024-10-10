2e6bab725cefd2bd1db5b86db963e85fbe699ae1
leminhtrungcn18b <leminhtrung1103@gmail.com>
2024-10-10 15:02:53 +0700

    setup jsp

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -19,10 +19,10 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping("/hoidanit")
+    @RequestMapping("/")
     public String getHomePage() {
         String test = this.userService.handleHello();
-        return "eric.html";
+        return "hello";
     }
 }
 
